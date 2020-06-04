79f64c0eb4dbbe635e9225bdc372602141ee2856
polar <polar.hicks61@gmail.com>
2020-06-03 18:06:24 -0600

    The last commit message (forgot to select staged changes)

---
+++ TextGame/src/textGame/Entity.java
@@ -0,0 +1,21 @@
+package textGame;
+
+public class Entity {
+	private String name, des;
+	
+	public void setName(String s) {
+		name = s;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setDes(String s) {
+		des = s;
+	}
+	
+	public String getDes() {
+		return des;
+	}
+}
